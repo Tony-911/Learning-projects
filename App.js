66a79b3985f2ee989a01d391e91e54c3f6173253
Tony <58776835+Tony-911@users.noreply.github.com>
2020-11-08 16:16:00 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,111 +1,42 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            firstName: "",
-            lastName: "",
-            isFriendly: false,
-            gender: "",
-            favColor: "blue"
-        }
-        this.handleChange = this.handleChange.bind(this)
+function App() {
+    const [inputData, setInputData] = useState({firstName: "", lastName: ""})
+    const [contactsData, setContactsData] = useState([])
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setInputData(prevInputData => ({...prevInputData, [name]: value}))
     }
     
-    handleChange(event) {
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+    function handleSubmit(event) {
+        event.preventDefault()
+        setContactsData(prevContacts => [...prevContacts, inputData])
     }
     
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
+    const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>{contact.firstName} {contact.lastName}</h2>)
+    
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
                 <input 
-                    type="text" 
-                    value={this.state.firstName} 
+                    placeholder="First Name"
                     name="firstName" 
-                    placeholder="First Name" 
-                    onChange={this.handleChange} 
+                    value={inputData.firstName}
+                    onChange={handleChange}
                 />
-                <br />
                 <input 
-                    type="text" 
-                    value={this.state.lastName} 
+                    placeholder="Last Name"
                     name="lastName" 
-                    placeholder="Last Name" 
-                    onChange={this.handleChange} 
+                    value={inputData.lastName}
+                    onChange={handleChange}
                 />
-                
-                {
-                    /**
-                     * Other useful form elements:
-                     * 
-                     *  <textarea /> element
-                     *  <input type="checkbox" />
-                     *  <input type="radio" />
-                     *  <select> and <option> elements
-                     */
-                }
-                
-                <textarea 
-                    value={"Some default value"}
-                    onChange={this.handleChange}
-                />
-                
                 <br />
-                
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="isFriendly"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Is friendly?
-                </label>
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="male"
-                        checked={this.state.gender === "male"}
-                        onChange={this.handleChange}
-                    /> Male
-                </label>
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="female"
-                        checked={this.state.gender === "female"}
-                        onChange={this.handleChange}
-                    /> Female
-                </label>
-                {/* Formik */}
-                <br />
-                
-                <label>Favorite Color:</label>
-                <select 
-                    value={this.state.favColor}
-                    onChange={this.handleChange}
-                    name="favColor"
-                >
-                    <option value="blue">Blue</option>
-                    <option value="green">Green</option>
-                    <option value="red">Red</option>
-                    <option value="orange">Orange</option>
-                    <option value="yellow">Yellow</option>
-                </select>
-                
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
-                <h2>You are a {this.state.gender}</h2>
-                <h2>Your favorite color is {this.state.favColor}</h2>
-                <button>Submit</button>
+                <button>Add contact</button>
             </form>
-        )
-    }
+            {contacts}
+        </>
+    )
 }
 
 export default App
